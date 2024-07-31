761c7ca09f9e67311259b40694acb537c53bdf6e
Christian Saravia <97811627+Houseofusher1996@users.noreply.github.com>
2024-07-30 21:38:41 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // smooth scroll
+  const links = document.querySelectorAll('nav a');
+  links.forEach(link => {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({behavior: 'smooth'});
+    });
+  });
+});
